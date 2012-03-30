0f4970e810797d86d570371710926ae8406ba2d4
grantpexsa <grant.pexsa@gmail.com>
2012-03-30 16:28:47 -0500

    created project structure

---
+++ src/com/Pexsa/Phonegap/PhoneGapAppActivity.java
@@ -0,0 +1,13 @@
+package com.Pexsa.Phonegap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhoneGapAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
